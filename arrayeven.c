9b93f94f883633bc11b3680c566a59d82a07e7a7
BhavyaBibi <bhavyabibi007@gmail.com>
2022-05-12 15:23:25 +0400

    array even numbers

---
+++ arrayeven.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+int main()
+{
+int size,values[10],i;
+printf(" enter the size of an array:");
+scanf("%d",&size);
+printf(" enter the values of array:");
+for(i=0; i<size; i++){
+    scanf("%d",&values[i]);
+    
+}
+
+printf(" all array elements are :");
+for(i=0; i<size; i++){
+    if (i%2==0){
+        printf("%d \t",values[i]);
+        
+    }
+    
+}
+    return 0;
+}
