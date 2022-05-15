38a85519718817d377dd224b72c5da1957f5a0c4
BhavyaBibi <bhavyabibi007@gmail.com>
2022-05-15 23:09:03 +0400

    the done project is added

--- functionwithoutargumentwithretunvalue.c
+++ functionwithoutargumentwithretunvalue.c
@@ -3,20 +3,28 @@
 #include<stdlib.h>
 
 //function without argument with return value
+
 int sum(); //declaration
-int main(void){
- int res;
- res=sum(); //function call
-    printf("enter the two number sum is :%d",res);
-    return 0;
+
+int main()
+{
+    int res;
     
+    res = sum();//function call
+     printf(" enter the two number sum is :%d",res);
+
+  return 0;
 }
 
+// function definition
 int sum(){
     int num1,num2,result;
-    printf(" enter the two numbers:");
+    printf("enter the 2 numbers:");
     scanf("%d%d",&num1,&num2);
+    
     result =num1+num2;
+    
+    
+   
     return result;
-
 }
