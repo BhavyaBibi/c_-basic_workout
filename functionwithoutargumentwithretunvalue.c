98aa214809021ec4f640f28a7f7640850e20c2fc
BhavyaBibi <bhavyabibi007@gmail.com>
2022-05-15 23:05:30 +0400

    function without argument withreturn value

---
+++ functionwithoutargumentwithretunvalue.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+#include<stdlib.h>
+
+//function without argument with return value
+
+int sum(); //declaration
+
+int main()
+{
+    int res;
+    
+    res = sum();//function call
+     printf(" enter the two number sum is :%d",res);
+
+  return 0;
+}
+
+// function definition
+int sum(){
+    int num1,num2,result;
+    printf("enter the 2 numbers:");
+    scanf("%d%d",&num1,&num2);
+    
+    result =num1+num2;
+    
+    
+   
+    return result;
+}
